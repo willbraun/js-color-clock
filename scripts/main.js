aa88356fbd71159c64dc8c2f686feb15f8f5a7a6
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-04 16:18:58 -0400

    separated update function so it can be called at page start and in event listeners

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const clockDisplay = document.querySelector('.clock-display');
+let showHex = false;
+
 // This function converts a date object to an array of [R,G,B] hex string values
 // It includes a function to convert a number to hexadecimal. 
 // It scales the number to 255 over a multiplier (24 or 60 usually) based on if you are using hours, minutes, or seconds.
@@ -10,17 +13,23 @@ const dateToArrayOfRGB = (date) => {
     return [numToHex(date.getHours(),24),numToHex(date.getMinutes(),60),numToHex(date.getSeconds(),60)];
 }
 
-let clockDisplay = document.querySelector('.clock-display');
-let showHex = false;
-
-const newTime = setInterval(function(){
-    let now = new Date();
+const updateDisplay = () => { 
+    let now = new Date;
     let timeDisplay = now.toTimeString().slice(0,8);
     let hexDisplay = dateToArrayOfRGB(now).join(':');
     clockDisplay.innerText = showHex ? hexDisplay : timeDisplay;
     document.querySelector('.clock-progress-bar').style.width = `${(now.getSeconds()*14/60).toFixed(2)}rem`;
     document.querySelector('.clock').style.background = `#${dateToArrayOfRGB(now).join('')}`;
-},1000);
+}
 
-clockDisplay.addEventListener('mouseover', () => showHex = true);
-clockDisplay.addEventListener('mouseout', () => showHex = false);
+updateDisplay();
+setInterval(updateDisplay,1000);
+
+clockDisplay.addEventListener('mouseover', () => {
+    showHex = true;
+    updateDisplay();
+});
+clockDisplay.addEventListener('mouseout', () => {
+    showHex = false;
+    updateDisplay();
+});
