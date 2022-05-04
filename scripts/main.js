a1f69fdd82ca5e475a8cdd523d64397b14e4e867
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-04 16:34:57 -0400

    wrapped code in iife so showhex cannot be updated from console and cleaned up update function so it only calculates the display value that is needed instead of both

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,39 @@
-const clockDisplay = document.querySelector('.clock-display');
-let showHex = false;
+// Wrapped everything in an IIFE so that showHex cannot be set from console, 
+// and so showHex is in the same scope as the later functions.
+(function(){
+    const clockDisplay = document.querySelector('.clock-display');
+    let showHex = false
 
-// This function converts a date object to an array of [R,G,B] hex string values
-// It includes a function to convert a number to hexadecimal. 
-// It scales the number to 255 over a multiplier (24 or 60 usually) based on if you are using hours, minutes, or seconds.
-// It does this so the possible numbers in each field cover the full range of RGB values
-const dateToArrayOfRGB = (date) => {
-    const numToHex = (num,multiplier) => {
-        let value = Math.floor(num*255/multiplier).toString(16).toUpperCase();
-        return value.length === 2 ? value : `0${value}`;
+    // This function converts a date object to an array of [R,G,B] hex string values
+    // It includes a function to convert a number to hexadecimal. 
+    // It scales the number to 255 over a multiplier (24 or 60 usually) based on if you are using hours, minutes, or seconds.
+    // It does this so the possible numbers in each field cover the full range of RGB values
+    const dateToArrayOfRGB = (date) => {
+        const numToHex = (num,multiplier) => {
+            let value = Math.floor(num*255/multiplier).toString(16).toUpperCase();
+            return value.length === 2 ? value : `0${value}`;
+        }
+        return [numToHex(date.getHours(),24),numToHex(date.getMinutes(),60),numToHex(date.getSeconds(),60)];
     }
-    return [numToHex(date.getHours(),24),numToHex(date.getMinutes(),60),numToHex(date.getSeconds(),60)];
-}
 
-// This function handles all of the actions that occur when the display should update
-const updateDisplay = () => { 
-    let now = new Date;
-    let timeDisplay = now.toTimeString().slice(0,8);
-    let hexDisplay = dateToArrayOfRGB(now).join(':');
-    clockDisplay.innerText = showHex ? hexDisplay : timeDisplay;
-    document.querySelector('.clock-progress-bar').style.width = `${(now.getSeconds()*14/60).toFixed(2)}rem`;
-    document.querySelector('.clock').style.background = `#${dateToArrayOfRGB(now).join('')}`;
-}
+    // This function handles all of the actions that occur when the display should update
+    const updateDisplay = () => { 
+        let now = new Date;
+        clockDisplay.innerText = showHex ? dateToArrayOfRGB(now).join(':') : now.toTimeString().slice(0,8);
+        document.querySelector('.clock-progress-bar').style.width = `${(now.getSeconds()*14/60).toFixed(2)}rem`;
+        document.querySelector('.clock').style.background = `#${dateToArrayOfRGB(now).join('')}`;
+    }
 
-updateDisplay();
-setInterval(updateDisplay,1000);
-
-clockDisplay.addEventListener('mouseover', () => {
-    showHex = true;
     updateDisplay();
-});
-clockDisplay.addEventListener('mouseout', () => {
-    showHex = false;
-    updateDisplay();
-});
+    setInterval(updateDisplay,1000);
+
+    clockDisplay.addEventListener('mouseover', () => {
+        showHex = true;
+        updateDisplay();
+    });
+    clockDisplay.addEventListener('mouseout', () => {
+        showHex = false;
+        updateDisplay();
+    });
+
+})();
