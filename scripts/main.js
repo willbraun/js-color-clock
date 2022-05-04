c54b95bc1556d1f1576c39c6513a7af3b302d73d
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-04 14:34:23 -0400

    updated date to hex code function to output array of RGB values so it can be formatted correctly for display and for updating the style.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-// This function converts a date object to a hex code.
+// This function converts a date object to an array of [R,G,B] hex string values
 // It includes a function to convert a number to hexadecimal. 
 // It scales the number to 255 over a multiplier (24 or 60 usually) based on if you are using hours, minutes, or seconds.
 // It does this so the possible numbers in each field cover the full range of RGB values
-const dateToHexCode = (date) => {
+const dateToArrayOfRGB = (date) => {
     const numToHex = (num,multiplier) => {
-        let value = Math.floor(num*255/multiplier).toString(16);
+        let value = Math.floor(num*255/multiplier).toString(16).toUpperCase();
         return value.length === 2 ? value : `0${value}`;
     }
-    return `${numToHex(date.getHours(),24)}${numToHex(date.getMinutes(),60)}${numToHex(date.getSeconds(),60)}`
+    return [numToHex(date.getHours(),24),numToHex(date.getMinutes(),60),numToHex(date.getSeconds(),60)];
 }
 
 let clockDisplay = document.querySelector('.clock-display');
@@ -16,7 +16,6 @@ const newTime = setInterval(function(){
     let now = new Date();
     clockDisplay.innerText = now.toTimeString().slice(0,8);
     document.querySelector('.clock-progress-bar').style.width = `${(now.getSeconds()/60)*14}rem`;
-    document.querySelector('.clock').style.background = `#${dateToHexCode(now)}`;
+    document.querySelector('.clock').style.background = dateToArrayOfRGB(now).join('');
+    let hover = clockDisplay.addEventListener('mouseover', e => e.target.innerText = dateToArrayOfRGB(now).join(':'));
 },1000);
-
-console.dir(document.querySelector('.clock-progress-bar'));
